8fd4b7e5866586d964866fa3e8a12194c838b2f6
EthanRule <ethan2002@live.com>
2025-05-20 11:44:26 -0700

    DatabaseEngine and user input for high level operations

--- database/src/main.rs
+++ database/src/main.rs
@@ -23,12 +23,50 @@ fn is_valid_json(json_str: &str) -> String {
     }
 }
 
-fn main() {
-    let valid_json = r#"{"name": "John", "age": 30}"#;
-    let invalid_json = r#"{"name": "John", "age": }"#;
+// Information expert database engine that holds a vector
+// of databases and performs high level operations like
+// creating, deleting, viewing databases.
+struct DatabaseEngine {
+    databases: Vec<Database>,
+}
 
-    let document = Document { id: 0i64, contents: is_valid_json(valid_json) };
-    let invalid_document = Document { id: 0i64, contents: is_valid_json(invalid_json) };
+impl DatabaseEngine {
+    fn create(&mut self) {
+        println!("creating database");
+    }
+
+    fn view(&self) {
+        println!("viewing databases");
+    }
+
+    fn destroy(&mut self) {
+        println!("destorying database");
+    } 
+}
+
+fn run() {
+    use text_io::read;
+    let mut database_engine = DatabaseEngine { databases: Vec::<Database>::new() };
+    loop {
+        // Process user input and forward input to it's corrolated function.
+        let input: String = read!();
+        println!("input: {:?}", input);
+        match input.as_str() {
+            "create" => database_engine.create(),
+            "view" => database_engine.view(),
+            "destroy" => database_engine.destroy(),
+            "exit" => return,
+            _ => println!("unknown command. Try create, view, destroy or exit instead"),
+        }
+    }
+}
+
+fn main() {
+    //let valid_json = r#"{"name": "John", "age": 30}"#;
+    //let invalid_json = r#"{"name": "John", "age": }"#;
+
+    //let document = Document { id: 0i64, contents: is_valid_json(valid_json) };
+    //let invalid_document = Document { id: 0i64, contents: is_valid_json(invalid_json) };
     
-    println!("{} {}", document.id, document.contents);
+    run();
 }
